3fe3122b765e9b8da400032ea71b529cf0132f68
goldenstar07 <ssinezdev07@gmail.com>
2021-12-02 01:28:14 +0800

    Texts

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -29,12 +29,10 @@ export default function Landing() {
               <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
                 <div className="pr-12">
                   <h1 className="text-white font-semibold text-5xl">
-                    Your story starts with us.
+                    Discover, collect, and sell extraordinary NFTs
                   </h1>
                   <p className="mt-4 text-lg text-blueGray-200">
-                    This is a simple example of a Landing Page you can build
-                    using Notus React. It features multiple CSS components based
-                    on the Tailwind CSS design system.
+                    on the world's first & largest NFT marketplace
                   </p>
                 </div>
               </div>
