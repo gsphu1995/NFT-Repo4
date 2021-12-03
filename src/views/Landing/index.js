60289712f0777b1098cb85bb7efebced84e9ce8e
goldenstar07 <ssinezdev07@gmail.com>
2021-12-03 19:11:46 +0800

    langing page design

--- src/views/Landing/index.js
+++ src/views/Landing/index.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
+import TopCollections from "./components/TopCollections";
 
 import useViewModel from "./methods";
 
@@ -63,7 +64,7 @@ export default function Landing() {
                     </div>
                 </div>
 
-                <section className="pb-20 bg-blueGray-200 -mt-24">
+                <section className="pb-40 bg-blueGray-200 -mt-24">
                     <div className="container mx-auto px-4">
                         <div className="flex flex-wrap">
                             <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
@@ -72,10 +73,9 @@ export default function Landing() {
                                         <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
                                             <i className="fas fa-award"></i>
                                         </div>
-                                        <h6 className="text-xl font-semibold">Awarded Agency</h6>
+                                        <h6 className="text-xl font-semibold">Set up your wallet</h6>
                                         <p className="mt-2 mb-4 text-blueGray-500">
-                                            Divide details about your product or agency work into
-                                            parts. A paragraph describing a feature will be enough.
+                                            Once you’ve set up your wallet of choice, connect it to OpenSea by clicking the wallet icon in the top right corner..
                                         </p>
                                     </div>
                                 </div>
@@ -87,10 +87,12 @@ export default function Landing() {
                                         <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
                                             <i className="fas fa-retweet"></i>
                                         </div>
-                                        <h6 className="text-xl font-semibold">Free Revisions</h6>
+                                        <h6 className="text-xl font-semibold">Create your collection / NFTS</h6>
                                         <p className="mt-2 mb-4 text-blueGray-500">
-                                            Keep you user engaged by providing meaningful information.
-                                            Remember that by this time, the user is curious.
+                                            Click My Collections and set up your collection. Add social links, a description, profile & banner images, and set a secondary sales fee.
+                                        </p>
+                                        <p className="text-blueGray-500">
+                                            Upload your work (image, video, audio, or 3D art), add a title and description, and customize your NFTs with properties, stats, and unlockable content.
                                         </p>
                                     </div>
                                 </div>
@@ -102,71 +104,21 @@ export default function Landing() {
                                         <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
                                             <i className="fas fa-fingerprint"></i>
                                         </div>
-                                        <h6 className="text-xl font-semibold">Verified Company</h6>
+                                        <h6 className="text-xl font-semibold">List them for sale</h6>
                                         <p className="mt-2 mb-4 text-blueGray-500">
-                                            Write a few lines about each one. A paragraph describing a
-                                            feature will be enough. Keep you user engaged!
+                                            Choose between auctions, fixed-price listings, and declining-price listings. You choose how you want to sell your NFTs, and we help you sell them!
                                         </p>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                        <div className="flex flex-wrap items-center mt-32">
-                            <div className="w-full md:w-5/12 px-4 mr-auto ml-auto">
-                                <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-white">
-                                    <i className="fas fa-user-friends text-xl"></i>
-                                </div>
-                                <h3 className="text-3xl mb-2 font-semibold leading-normal">
-                                    Working with us is a pleasure
-                                </h3>
-                                <p className="text-lg font-light leading-relaxed mt-4 mb-4 text-blueGray-600">
-                                    Don't let your uses guess by attaching tooltips and popoves to
-                                    any element. Just make sure you enable them first via
-                                    JavaScript.
-                                </p>
-                                <p className="text-lg font-light leading-relaxed mt-0 mb-4 text-blueGray-600">
-                                    The kit comes with three pre-built pages to help you get
-                                    started faster. You can change the text and images and you're
-                                    good to go. Just make sure you enable them first via
-                                    JavaScript.
-                                </p>
-                                <Link to="/" className="font-bold text-blueGray-700 mt-8">
-                                    Check Notus React!
-                                </Link>
-                            </div>
-
-                            <div className="w-full md:w-4/12 px-4 mr-auto ml-auto">
-                                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-lightBlue-500">
-                                    <img
-                                        alt="..."
-                                        src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"
-                                        className="w-full align-middle rounded-t-lg"
-                                    />
-                                    <blockquote className="relative p-8 mb-4">
-                                        <svg
-                                            preserveAspectRatio="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 583 95"
-                                            className="absolute left-0 w-full block h-95-px -top-94-px"
-                                        >
-                                            <polygon
-                                                points="-30,95 583,95 583,65"
-                                                className="text-lightBlue-500 fill-current"
-                                            ></polygon>
-                                        </svg>
-                                        <h4 className="text-xl font-bold text-white">
-                                            Top Notch Services
-                                        </h4>
-                                        <p className="text-md font-light mt-2 text-white">
-                                            The Arctic Ocean freezes every winter and much of the
-                                            sea-ice then thaws every summer, and that process will
-                                            continue whatever happens.
-                                        </p>
-                                    </blockquote>
-                                </div>
-                            </div>
+                        <TopCollections/>
+                        <div className="text-center mt-6">
+                            <button className="bg-lightBlue-500 rounded-lg px-4 py-3 text-white font-bold text-lg">Go to Rankings</button>
                         </div>
+
+
                     </div>
                 </section>
 
